8141853449ab7a742c9df47b0b8f39697d466be9
Team-Cognition <91435353+Team-Cognition@users.noreply.github.com>
2022-11-22 18:10:29 -0800

    clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleoptest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleoptest.java
@@ -25,17 +25,10 @@ public class Teleoptest extends LinearOpMode {
 
     // Declare variables
     boolean lastA = false;                      // Use to track the prior button state.
-    double servo_duckspinner_power;
     boolean secondHalf = false;                 // Use to hint the drivers for end game start
     final double HALF_TIME = 60.0;              // Wait this many seconds before alert for half-time
     ElapsedTime runtime = new ElapsedTime();    // Use to determine when end game is starting.
 
-    static final double SERVO_DUCKSPINNER_STOP = 0;
-    static final double SERVO_DUCKSPINNER_SPIN_FORWARD = 1;
-    static final double SERVO_DUCKSPINNER_SPIN_REVERSE = -1;
-    static final double SERVO_ARM_STOP = 0;
-    static final double SERVO_ARM_UP = 0.25;
-    static final double SERVO_ARM_DOWN = -0.25;
     static final double MOTOR_TICK_COUNT = 1120;
     static final double MAX_POS = 1.0;     // Maximum rotational position for gripper
     static final double MIN_POS = 0.0;     // Minimum rotational position for gripper
@@ -49,46 +42,26 @@ public class Teleoptest extends LinearOpMode {
     public final double armpower = 0.5;
     public final double armpower2 = -0.5;
 
-    Gamepad.RumbleEffect customRumbleEffect;    // Use to build a custom rumble sequence.
-
     public void runOpMode() throws InterruptedException {
 
         motorFrontLeft = hardwareMap.dcMotor.get("upperLeft"); //motorFrontLeft
         motorBackLeft = hardwareMap.dcMotor.get("lowerLeft"); //motorBackLeft
         motorFrontRight = hardwareMap.dcMotor.get("upperRight"); //motorFrontRight
         motorBackRight = hardwareMap.dcMotor.get("lowerRight"); //motorBackRight
-        servoArm = hardwareMap.dcMotor.get("arm"); //arm
         claw = hardwareMap.servo.get("claw");
-        //servoDuckSpinner = hardwareMap.get(Servo.class, "duckSpinner");
-        //servoDuckSpinner = hardwareMap.get(Servo.class,"duckSpinner");
-        //servoRightClaw = hardwareMap.get(Servo.class, "rightClaw");
-        //gripServo = hardwareMap.get(Servo.class, "leftClaw");
+        arm = hardwareMap.dcMotor.get("arm"); //Calling the arm
+
 
         double quarterTurn = MOTOR_TICK_COUNT / 4;
-        //servoArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //servoArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //sets the counter of ticks to 0
-        //servoArm.setZeroPowerBehavior(DcMotor.ZeroPowerBrake.BRAKE);
 
-        customRumbleEffect = new Gamepad.RumbleEffect.Builder()
-                .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
-                .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
-                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
-                .addStep(0.0, 0.0, 250)  //  Pause for 250 mSec
-                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
-                .build();
-
-
-        servo_duckspinner_power = SERVO_DUCKSPINNER_STOP;
-
-        //servoDuckSpinner.setPower(servo_duckspinner_pos);
 
         //Reverse front right and back right motors
      //  motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
      //   motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
          motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        //servoArm.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("TeleOp>", "Press Start");
         telemetry.update();
@@ -103,17 +76,12 @@ public class Teleoptest extends LinearOpMode {
         armPosition = .5;                   // set arm to half way up.
         gripPosition = MAX_POSITION;        // set grip to full open.
 
-        arm = hardwareMap.dcMotor.get("arm"); //Calling the arm
-        arm.setDirection(DcMotorSimple.Direction.FORWARD);
-
-
         while (opModeIsActive()) {
             double y = gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
             if ((runtime.seconds() > HALF_TIME) && !secondHalf) {
-                gamepad1.runRumbleEffect(customRumbleEffect);
                 secondHalf = true;
             }
 
@@ -135,7 +103,7 @@ public class Teleoptest extends LinearOpMode {
             }
             if (gamepad2.right_trigger > 0.0 && gripPosition > MIN_POSITION) {
               gripPosition=gripPosition-0.01;
-                telemetry.addData("bumperRight", "hi");
+              telemetry.addData("bumperRight", "hi");
 
             }
             telemetry.addData("gripPosition", gripPosition);
